9231004ecd4f9c88a019c904b34fe8d73e188b88
Vanca Rafael <vanca.rafael.marian@gmail.com>
2023-01-08 18:28:14 +0200

    Adding project

---
+++ public/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\src\\\\index.js: Identifier 'AnimalCard' has already been declared. (28:9)\\n\\n\\u001b[0m \\u001b[90m 26 |\\u001b[39m }\\u001b[0m\\n\\u001b[0m \\u001b[90m 27 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 28 |\\u001b[39m \\u001b[36mfunction\\u001b[39m \\u001b[33mAnimalCard\\u001b[39m(props) {\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m          \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 29 |\\u001b[39m     \\u001b[36mreturn\\u001b[39m \\u001b[33m<\\u001b[39m\\u001b[33mp\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33mHi\\u001b[39m\\u001b[33m,\\u001b[39mmy name is {props\\u001b[33m.\\u001b[39mname} and \\u001b[33mI\\u001b[39m am {props\\u001b[33m.\\u001b[39mspecies}\\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mp\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 30 |\\u001b[39m }\\u001b[0m\\n\\u001b[0m \\u001b[90m 31 |\\u001b[39m\\u001b[0m\\n    at instantiate (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:67:32)\\n    at constructor (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:364:12)\\n    at JSXParserMixin.raise (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3363:19)\\n    at ScopeHandler.checkRedeclarationInScope (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1587:19)\\n    at ScopeHandler.declareName (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1558:12)\\n    at JSXParserMixin.registerFunctionStatementId (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13734:16)\\n    at JSXParserMixin.parseFunction (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13717:12)\\n    at JSXParserMixin.parseFunctionStatement (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13342:17)\\n    at JSXParserMixin.parseStatementContent (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12996:21)\\n    at JSXParserMixin.parseStatementLike (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12963:17)\\n    at JSXParserMixin.parseModuleItem (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12944:17)\\n    at JSXParserMixin.parseBlockOrModuleBlockBody (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13569:36)\\n    at JSXParserMixin.parseBlockBody (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13561:10)\\n    at JSXParserMixin.parseProgram (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12853:10)\\n    at JSXParserMixin.parseTopLevel (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12843:25)\\n    at JSXParserMixin.parse (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14751:10)\\n    at parse (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14793:38)\\n    at parser (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\parser\\\\index.js:41:34)\\n    at parser.next (<anonymous>)\\n    at normalizeFile (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\normalize-file.js:65:38)\\n    at normalizeFile.next (<anonymous>)\\n    at run (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\index.js:21:50)\\n    at run.next (<anonymous>)\\n    at transform (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transform.js:22:41)\\n    at transform.next (<anonymous>)\\n    at step (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\gensync\\\\index.js:261:32)\\n    at C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\chita\\\\OneDrive\\\\Desktop\\\\mern-project\\\\node_modules\\\\gensync\\\\index.js:223:11)\");\n\n//# sourceURL=webpack://mern-project/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
